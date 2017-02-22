93f24de1619aa8ca96f531f53e240f962f826abb
exeetor <squall_haj@hotmail.com>
2017-02-22 21:02:32 +0100

    updated with engines

---
+++ Assets/Code/Shared/Engine.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Engine : MonoBehaviour
+{
+    [SerializeField]
+    private float mass;
+    [SerializeField]
+    [Range(0,10)]
+    private float thrust;
+    [SerializeField]
+    private bool toggle;
+    [SerializeField]
+    private float MaxThrust;
+    [SerializeField]
+    private float acceleration;
+
+    #region Properties
+
+    public float Mass
+    {
+        get
+        {
+            return mass;
+        }
+    }
+
+    public float Thrust
+    {
+        get
+        {
+            return thrust;
+        }
+
+        set
+        {
+            thrust = value;
+        }
+    }
+
+    public bool Toggle
+    {
+        get
+        {
+            return toggle;
+        }
+
+        set
+        {
+            toggle = value;
+        }
+    }
+
+    public float Acceleration
+    {
+        get
+        {
+            return acceleration;
+        }
+    }
+
+    #endregion
+
+    private void Update()
+    {
+        if (Toggle)
+        {
+            GetComponentInParent<Rigidbody2D>().AddForce(-transform.right * Thrust);
+        }
+    }
+}
--- Assets/Code/Shared/Movement.cs
+++ Assets/Code/Shared/Movement.cs
@@ -14,8 +14,15 @@ public class Movement : MonoBehaviour
     private bool leftThrustToggle = false;
     private float rightThrust = 0.0f;
     private bool rightThrustToggle = false;
-
-    private float rotationSpeed = 1.0f;
+    [SerializeField]
+    private Engine frontEngine;
+    [SerializeField]
+    private Engine backEngine;
+    [SerializeField]
+    private Engine leftEngine;
+    [SerializeField]
+    private Engine rightEngine;
+    
 
     private Rigidbody2D body;
 
@@ -88,6 +95,57 @@ public class Movement : MonoBehaviour
         }
     }
 
+    public Engine FrontEngine
+    {
+        get
+        {
+            return frontEngine;
+        }
+
+        set
+        {
+            frontEngine = value;
+        }
+    }
+
+    public Engine BackEngine
+    {
+        get
+        {
+            return backEngine;
+        }
+
+        set
+        {
+            backEngine = value;
+        }
+    }
+
+    public Engine LeftEngine
+    {
+        get
+        {
+            return leftEngine;
+        }
+
+        set
+        {
+            leftEngine = value;
+        }
+    }
+
+    public Engine RightEngine
+    {
+        get
+        {
+            return rightEngine;
+        }
+
+        set
+        {
+            rightEngine = value;
+        }
+    }
 
     #endregion
 
@@ -99,91 +157,93 @@ public class Movement : MonoBehaviour
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
+        body.mass = body.mass + backEngine.Mass + frontEngine.Mass + leftEngine.Mass + rightEngine.Mass;
     }
 
     void FixedUpdate()
     {
         float angle = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg;
-        Vector2 forceVector = new Vector2(Mathf.Cos(Mathf.Deg2Rad * angle), Mathf.Sin(Mathf.Deg2Rad * angle));
-        Vector2 forceBack = new Vector2(-forceVector.y, -forceVector.x);
-        Vector2 forceRight = new Vector2(forceVector.y, forceVector.x);
-        Vector2 forceLeft = new Vector2(-forceRight.y, -forceRight.x);
-        //transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
-
         body.rotation = angle;
 
         //calculate force
+        //calculateThrust();
+        FrontEngine.Toggle = BackThrustToggle;
+        BackEngine.Toggle = ForwardThrustToggle;
+        RightEngine.Toggle = RightThrustToggle;
+        LeftEngine.Toggle = LeftThrustToggle;
+        //calcForce(angle);
+        
+    }
 
-        if (ForwardThrustToggle)
-        {
-            if (ForwardThrust < 3)
-            {
-                ForwardThrust = ForwardThrust + 1f;
-            }
-        }
-        else if (ForwardThrust > 0)
-        { 
-            ForwardThrust = ForwardThrust - 1f;
-            if (forwardThrust < 0)
-                ForwardThrust = 0;
-        }
 
-        if (BackThrustToggle)
-        {
-            if (BackThrust < 2)
-            {
-                BackThrust = BackThrust + 0.5f;
-            }
-        }
-        else if (BackThrust > 0)
-        {
-            BackThrust = BackThrust - 1f;
-            if (BackThrust < 0)
-                BackThrust = 0;
-        }
-        if (RightThrustToggle)
-        {
-            if (RightThrust < 1)
-            {
-                RightThrust = RightThrust + 0.2f;
-            }
-        }
-        else if (RightThrust > 0)
-        {
-            RightThrust = RightThrust - 1f;
-            if (RightThrust < 0)
-                RightThrust = 0;
-        }
-        if (LeftThrustToggle)
-        {
-            if (LeftThrust < 1)
-            {
-                LeftThrust = LeftThrust + 0.2f;
-            }
-        }
-        else if (LeftThrust > 0)
-        {
-            LeftThrust = LeftThrust - 1f;
-            if (LeftThrust < 0)
-                LeftThrust = 0;
-        }
+
+    //void calculateThrust()
+    //{
+    //    if (ForwardThrustToggle)
+    //    {
+    //        if(ForwardThrust < 5)
+    //            ForwardThrust = ForwardThrust + ForwardEngine/body.mass;
+    //    }
+    //    else if (ForwardThrust > 0)
+    //    {
+    //        ForwardThrust = ForwardThrust - 1f;
+    //        if (forwardThrust < 0)
+    //            ForwardThrust = 0;
+    //    }
+
+    //    if (BackThrustToggle)
+    //    {
+    //        if (BackThrust < 2)
+    //        {
+    //            BackThrust = BackThrust + 1.0f/body.mass;
+    //        }
+    //    }
+    //    else if (BackThrust > 0)
+    //    {
+    //        BackThrust = BackThrust - 0.5f;
+    //        if (BackThrust < 0)
+    //            BackThrust = 0;
+    //    }
+    //    if (RightThrustToggle)
+    //    {
+    //        if (RightThrust < 1)
+    //        {
+    //            RightThrust = RightThrust + 0.5f;
+    //        }
+    //    }
+    //    else if (RightThrust > 0)
+    //    {
+    //        RightThrust = RightThrust - 0.25f;
+    //        if (RightThrust < 0)
+    //            RightThrust = 0;
+    //    }
+    //    if (LeftThrustToggle)
+    //    {
+    //        if (LeftThrust < 1)
+    //        {
+    //            LeftThrust = LeftThrust + 0.5f;
+    //        }
+    //    }
+    //    else if (LeftThrust > 0)
+    //    {
+    //        LeftThrust = LeftThrust - 0.25f;
+    //        if (LeftThrust < 0)
+    //            LeftThrust = 0;
+    //    }
+    //}
+
+    void calcForce(float angle)
+    {
+        Vector2 forceVector = new Vector2(Mathf.Cos(Mathf.Deg2Rad * angle), Mathf.Sin(Mathf.Deg2Rad * angle));
+        Vector2 forceBack = new Vector2(-forceVector.x, -forceVector.y);
+        Vector2 forceRight = new Vector2(Mathf.Cos(Mathf.Deg2Rad * (angle - 90)), Mathf.Sin(Mathf.Deg2Rad * (angle - 90)));
+        //Vector2 forceRight = new Vector2(forceVector.y, forceVector.x);
+        Vector2 forceLeft = new Vector2(-forceRight.x, -forceRight.y);
 
         body.AddForce(forceVector * ForwardThrust);
         body.AddForce(forceRight * RightThrust);
         body.AddForce(forceLeft * LeftThrust);
         body.AddForce(forceBack * BackThrust);
-
-
-
     }
-
-    //bool TrustersOn()
-    //{
-    //    if (forwardThrustToggle == true || backThrustToggle == true || rightThrustBoolean == true || leftThrustToggle == true)
-    //        return true;
-    //    return false;
-    //}
-
-
-    
+        
 }
