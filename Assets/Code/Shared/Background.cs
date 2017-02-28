3fe31088629a41e4076952417165d2d5395186f9
buggo <rickard.b.karlsson@gmail.com>
2017-02-28 14:53:03 +0100

    Camera follow player

---
+++ Assets/Code/Shared/Background.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour {
+
+    Material mat;
+    [Range(1,200)]
+    public float parallax = 1;
+
+	// Use this for initialization
+	void Start () {
+        mat = GetComponent<MeshRenderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        mat.mainTextureOffset = transform.position / 40f;
+    }
+}
---
+++ Assets/Code/Shared/GlueToTarget.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlueToTarget : MonoBehaviour {
+    public Transform target;
+	// Update is called once per frame
+	void Update () {
+        if (target != null)
+        {
+            transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+        }
+    }
+}
